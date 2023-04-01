d738683afc32b872abcf05c311bd25b1907a799a
RakeshDharne <rakeshbdharne103gmail.com>
2023-04-02 00:06:06 +0530

    ye sab naye file haii

---
+++ java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class java  {
+    public static void main(string args []) {
+        System.out.print("hello world");
+        
+    }
+    
+}
---
+++ one.java
@@ -0,0 +1,12 @@
+public class one{
+    public static void main(String[] args) {
+        System.out.print("hellow world");
+        System.out.print("hellow world");
+        System.out.print("hellow world\n");
+        System.out.println("hellow world");
+        System.out.println("hellow world");//iske baad nayi line
+        System.out.print("hellow world");
+
+
+    }
+}
